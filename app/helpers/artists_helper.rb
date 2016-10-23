eafd4638b74e246fadf1c9e102067ef980ca8e84
giuliogallerini <giulio.gallerini.work.com>
2016-10-23 15:44:58 +0200

    compleated Artists Index

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+
+  def genre artist
+    !artist.songs.nil? ? artist.songs[0].genre : "undefined"
+  end
+
+  def artist_photo artist
+    !artist.photo.nil? ? image_tag(artist.photo, class: "img-circle img-responsive") : image_tag("https://a2ua.com/singer/singer-003.jpg", class: "img-circle img-responsive")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015115831) do
+ActiveRecord::Schema.define(version: 20161023121516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "artists", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.text     "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "photo"
   end
 
   create_table "songs", force: :cascade do |t|
